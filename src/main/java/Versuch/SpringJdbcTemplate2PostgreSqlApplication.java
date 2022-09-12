54e09d559f9d031f287af6eabe544d080c3075ed
M-Kub <Tohrka@hotmail.com>
2022-09-12 22:30:28 +0200

    Added Database Connection and SQL-Statements to create the wanted tables

--- src/main/java/Versuch/SpringJdbcTemplate2PostgreSqlApplication.java
+++ src/main/java/Versuch/SpringJdbcTemplate2PostgreSqlApplication.java
@@ -21,16 +21,38 @@ public class SpringJdbcTemplate2PostgreSqlApplication {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
         //set the data source properties
 
-        org.postgresql.ds.PGSimpleDataSource d = new org.postgresql.ds.PGSimpleDataSource();
-        d.setUrl(url);
-        d.setUser(username);
-        d.setPassword(password);
-        jdbcTemplate.setDataSource(d);
+        org.postgresql.ds.PGSimpleDataSource ds = new org.postgresql.ds.PGSimpleDataSource();
+        ds.setUrl("jdbc:postgresql://free-tier13.aws-eu-central-1.cockroachlabs.cloud:26257/defaultdb?options=--cluster%3Dbkw-devices-3271&sslmode=verify-full&sslrootcert=C:\\Users\\M-Kub\\AppData\\Roaming\\postgresql\\root.crt");
+        ds.setUser("tohrka");
+        ds.setPassword("0qGV21S4DcrX-cs717Ooww");
+
+
+        jdbcTemplate.setDataSource(ds);
 
         //create a new table
         jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS " +
                 "Geraete (Seriennummer INT PRIMARY KEY, Marke STRING" +
-                ", Modell STRING, Schaeden STRING,Baujahr INT)");
+                ", Modell STRING, Schaeden STRING,Baujahr INT);" +
+                "CREATE TABLE IF NOT EXISTS " +
+                "Ausgabe (Schueler_ID INT, Seriennummer INT," +
+                " Ausgabe_ab DATE, Ausgabe_bis DATE," +
+                " PRIMARY KEY (Schueler_ID, Seriennummer));" +
+                "CREATE TABLE IF NOT EXISTS " +
+                "Schueler (Schueler_ID INT PRIMARY KEY, Nachname STRING," +
+                "Vorname String, Klassen_ID INT," +
+                "Geburtsdatum DATE, Adresse STRING," +
+                "Email STRING, Telefonnummer STRING);" +
+                "CREATE TABLE IF NOT EXISTS Klasse" +
+                "(Klassen_ID INT PRIMARY KEY, Klassenname STRING," +
+                "Lehrer_ID INT);" +
+                "CREATE TABLE IF NOT EXISTS Lehrer" +
+                "(Lehrer_ID INT PRIMARY KEY, Nachname STRING," +
+                "Vorname STRING, Email STRING," +
+                "Telefonnummer STRING,Admin BOOLEAN," +
+                "Adresse STRING);" +
+                "CREATE TABLE IF NOT EXISTS Login" +
+                "(Leher_ID INT PRIMARY KEY,Nutzername STRING," +
+                " Passwort STRING);");
 
 
     }
