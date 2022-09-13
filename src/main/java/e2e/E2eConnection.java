e16f38781d14cee4d173b9a840fe49e9ae6739db
Tohrka <tohrka@hotmail.com>
2022-09-13 12:18:13 +0200

    changed connection to a singleton class

--- src/main/java/e2e/E2eConnection.java
+++ src/main/java/e2e/E2eConnection.java
@@ -4,7 +4,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class E2eConnection
 {  private static final E2eConnection Con = new E2eConnection();
-    private String url = "jdbc:postgresql://free-tier13.aws-eu-central-1.cockroachlabs.cloud:26257/defaultd?sslmode=disable";
     private String username = "tohrka";
     private String password = "0qGV21S4DcrX-cs717Ooww";
     private JdbcTemplate jdbcTemplate = new JdbcTemplate();
@@ -13,8 +12,8 @@ public class E2eConnection
    private E2eConnection() {
        org.postgresql.ds.PGSimpleDataSource ds = new org.postgresql.ds.PGSimpleDataSource();
        ds.setUrl(path);
-       ds.setUser("tohrka");
-       ds.setPassword("0qGV21S4DcrX-cs717Ooww");
+       ds.setUser(username);
+       ds.setPassword(password);
        jdbcTemplate.setDataSource(ds);
    }
     public static E2eConnection getInstance() { return Con; }
